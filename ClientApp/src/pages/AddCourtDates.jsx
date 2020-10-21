98b36af675aeaee34683595276c23661c4cee52c
Byron McDaniel <byronkmcdaniel@gmail.com>
2020-10-20 20:39:01 -0400

    connecting to Api

--- ClientApp/src/pages/AddCourtDates.jsx
+++ ClientApp/src/pages/AddCourtDates.jsx
@@ -6,22 +6,69 @@ import { Header } from '../components/Header'
 
 export function AddCourtDates() {
   const [selectedDate, setSelectedDate] = useState(null)
+  const [newCourtDate, setNewCourtDate] = useState({
+    when: '',
+    caseNumber: '',
+    charge: '',
+    courtRoom: '',
+  })
+
+  function handleStringFieldChange(event) {
+    const value = event.target.value
+    const fieldName = event.target.name
+
+    const updatedCourtDate = { ...newCourtDate, [fieldName]: value }
+
+    setNewCourtDate(updatedCourtDate)
+  }
   return (
     <>
       <Header />
       <form className="Add">
-        <div className="Picker">
-          <h1>Please enter your court Date.</h1>
-          <h5>
-            IF YOU HAVE RECEIVED A COURT DATE BEFORE WE HAD A CHANCE TO GIVE IT
-            TO YOU PLEASE ENTER IT HERE
-          </h5>
+        <h1>Please enter your court Date.</h1>
+        <h5>
+          IF YOU HAVE RECEIVED A COURT DATE BEFORE WE HAD A CHANCE TO GIVE IT TO
+          YOU PLEASE ENTER IT HERE
+        </h5>
+        <div className="date">
           <label>Select a court date: </label>
           <DatePicker
+            name="when"
+            value={newCourtDate.when}
             selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
+            onChange={(date) => {
+              setSelectedDate(date)
+              setNewCourtDate({ ...newCourtDate, when: date })
+            }}
             dateFormat="yyyy-MM-dd"
             minDate={new Date()}
+          />
+        </div>
+        <div className="Case">
+          <label> Enter Case Number: </label>
+          <input
+            name="caseNumber"
+            type="caseNumber"
+            value={newCourtDate.caseNumber}
+            onChange={handleStringFieldChange}
+          />
+        </div>
+        <div className="Charge">
+          <label> Enter Charge: </label>
+          <input
+            name="charge"
+            type="Charge"
+            value={newCourtDate.charge}
+            onChange={handleStringFieldChange}
+          />
+        </div>
+        <div className="CourtRoom">
+          <label> Enter CourtRoom: </label>
+          <input
+            name="courtRoom"
+            type="courtRoom"
+            value={newCourtDate.courtRoom}
+            onChange={handleStringFieldChange}
           />
         </div>
       </form>
